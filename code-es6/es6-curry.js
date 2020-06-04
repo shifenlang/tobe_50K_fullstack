65ce920ac0e8e70da876e28be541c4b32e7700d4
Cheemi <xue.yuan.99999@gmail.com>
2020-06-04 11:27:47 +0800

    Create es6-curry.js

---
+++ code-es6/es6-curry.js
@@ -0,0 +1,6 @@
+const curry = (fn, ...args) =>
+    args.length < fn.length
+        //参数长度不足时，重新柯里化该函数，等待接受新参数
+        ? (...arguments) => curry(fn, ...args, ...arguments)
+        //参数长度满足时，执行函数
+        : fn(...args);
